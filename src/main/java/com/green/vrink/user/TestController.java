126a792b1417616c5f74be8f87dcbca378428611
gyuha <gyuha454@gmail.com>
2023-10-25 21:02:46 +0900

    테스트중

--- src/main/java/com/green/vrink/user/TestController.java
+++ src/main/java/com/green/vrink/user/TestController.java
@@ -1,8 +1,13 @@
 package com.green.vrink.user;
 
+import com.green.vrink.user.dto.TestDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -15,5 +20,15 @@ public class TestController {
 
         return "test";
     }
+    @PostMapping("/test2")
+    @ResponseBody
+    public ResponseEntity<?> test2(@RequestBody TestDTO testDTO){
+
+        log.info("Test Dto {}",testDTO);
+
+
+        return ResponseEntity.ok().build();
+    }
+
 
 }
---
+++ src/main/java/com/green/vrink/user/dto/TestDTO.java
@@ -0,0 +1,13 @@
+package com.green.vrink.user.dto;
+
+import lombok.Data;
+
+@Data
+
+public class TestDTO {
+
+    private String accountName;
+    private String accountNumber;
+
+
+}
