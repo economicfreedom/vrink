6328a388c708e39b83e64ce341a0ad3a58091a79
gyuha <gyuha454@gmail.com>
2023-10-25 15:24:08 +0900

    ㅅㄷㄴㅅ

---
+++ src/main/java/com/green/vrink/VrinkApplication.java
@@ -0,0 +1,13 @@
+package com.green.vrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/vrink/VrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.vrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
