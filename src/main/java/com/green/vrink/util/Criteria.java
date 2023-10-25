ae550839b5fe4e0fc31f906fb34783578efaad7a
gyuha <gyuha454@gmail.com>
2023-10-25 15:50:04 +0900

    ㅅㄷㄴㅅ

---
+++ src/main/java/com/green/vrink/util/Criteria.java
@@ -0,0 +1,37 @@
+package com.green.vrink.util;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+//페이징 크리테리아
+@Getter
+@Setter
+@ToString
+
+	/**
+	 * @author 최규하
+	 * 페이징 크리테리아 기본적인 형식을 따름
+	 */
+public class Criteria {
+	
+	//사용자가 선택한 페이지 정보를 담을 변수.
+
+	private int pageNum;
+	private int countPerPage;
+
+	//검색에 필요한 데이터를 변수로 선언.
+	private String keyword;
+	private String type;
+	private String orderBy;
+	public int getOffset() {
+    return (this.pageNum - 1) * this.countPerPage;
+}
+	public Criteria() {
+		this.pageNum = 1;
+		this.countPerPage = 10;
+	}
+
+}
---
+++ src/main/java/com/green/vrink/util/PageDTO.java
@@ -0,0 +1,47 @@
+package com.green.vrink.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+//페이징 DTO
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+	/**
+	 * @author 최규하
+	 * 기본적인 페이징 형식을 따름
+	 */
+public class PageDTO {
+	private Criteria cri;
+	private int articleTotalCount;
+	private int endPage;
+	private int beginPage;
+	private boolean prev;
+	private boolean next;
+	
+	private final int buttonNum = 5;
+	
+	
+	private void calcDataOfPage() {
+		
+		endPage = (int) (Math.ceil(cri.getPageNum() / (double) buttonNum) * buttonNum);
+		
+		beginPage = (endPage - buttonNum) + 1;
+		
+		prev = beginPage != 1;
+		
+		next = articleTotalCount > (endPage * cri.getCountPerPage());
+		
+		if(!next) {
+			endPage = (int) Math.ceil(articleTotalCount / (double) cri.getCountPerPage()); 
+		}
+		
+	}
+	
+	public void setArticleTotalCount(int articleTotalCount) {
+		this.articleTotalCount = articleTotalCount;
+		calcDataOfPage();
+	}
+}
