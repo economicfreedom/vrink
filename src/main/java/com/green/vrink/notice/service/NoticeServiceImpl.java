c21abc0acbf31e9b43886d91aee913a8dcf0f4ae
gyuha <gyuha454@gmail.com>
2023-11-13 16:00:33 +0900

    날라간거 수정

--- src/main/java/com/green/vrink/notice/service/NoticeServiceImpl.java
+++ src/main/java/com/green/vrink/notice/service/NoticeServiceImpl.java
@@ -1,4 +1,35 @@
 package com.green.vrink.notice.service;
 
-public class NoticeServiceImpl {
+import com.green.vrink.notice.dto.NoticeDto;
+import com.green.vrink.notice.repository.interfaces.NoticeRepository;
+import com.green.vrink.util.Criteria;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class NoticeServiceImpl implements NoticeService{
+    private final NoticeRepository noticeRepository;
+    @Override
+    public Integer getTotal(Criteria criteria) {
+        return noticeRepository.getTotal(criteria);
+    }
+
+    @Override
+    public List<NoticeDto> getNoticeList(Criteria criteria) {
+        return noticeRepository.findAllByCriteria(criteria);
+    }
+
+    @Override
+    public NoticeDto getNotice(int noticeId) {
+        return noticeRepository.findByNoticeId(noticeId);
+    }
+        @Override
+    public List<NoticeDto> noticeList(String type) {
+        return noticeRepository.findNoticeListByType(type);
+    }
 }
